bd1c9c9bf0d57c57f1b73697a0717e19bb5d7966
Ighoorr <80061981+Ighoorr@users.noreply.github.com>
2021-10-18 23:51:26 +0300

    Add files via upload

---
+++ Task 7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "I go to school. Girl runs to school";
+            Vocabulary my = new Vocabulary();
+            sentence=my.change(sentence);
+            Console.WriteLine('\n');
+            Console.WriteLine(sentence);
+
+        }
+    }
+}
---
+++ Task 7/Vocabulary.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ7
+{
+    class Vocabulary
+    {
+        Dictionary<string, string> data;
+
+        public Vocabulary()
+        {
+            data = new Dictionary<string, string>();
+            data.Add("I", "Boy");
+            data.Add("go", "run");
+            data.Add("to", "to");
+            data.Add("school", "cinema");
+        }
+        public bool AddNew(string str)
+        {
+            if (check(str) == false)
+            {
+                string temp;
+                Console.WriteLine($"Write word substitude:{str}");
+                temp=Console.ReadLine();
+                data.Add(str, temp);
+                return true;
+            }
+            return false;
+        }
+        public bool check(string str)
+        {
+            return data.ContainsKey(str);
+        }
+        public string changeword(string str)
+        {
+            return data[str];
+        }
+        public string change(string text)
+        {
+            string temp = "";
+            string[] sentences = text.Split(new char[] { '.' }, StringSplitOptions.RemoveEmptyEntries);
+            
+            for (int i = 0; i < sentences.Length; i++)
+            {
+                string[] words = sentences[i].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                for(int j = 0; j < words.Length; j++)
+                {
+                    if (check(words[j]) == false) AddNew(words[j]);
+                    temp += changeword(words[j])+' ';
+                }
+                temp += '.';
+ 
+            }
+
+            return temp;
+        }
+
+    }
+}
