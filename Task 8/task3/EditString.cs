720c8c54e90558a9199bd785a0972ed0e3b42e17
Ighoorr <80061981+Ighoorr@users.noreply.github.com>
2021-10-26 01:29:39 +0300

    Add files via upload

---
+++ Task 8/task3/EditString.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+    class EditString
+    {
+
+        public static string MostBrackets(string filePath)
+        {
+            StreamReader sr =new StreamReader(filePath);
+            
+
+
+            string result = "";
+            string current = "";
+            bool setNewSentance = false;
+
+            int max = 0;
+            int count = 0;
+
+            string sentence = sr.ReadLine();
+            while (sentence != null)
+            {
+                foreach (char letter in sentence)
+                {
+                    if (letter == '.')
+                    {
+                        current += letter;
+                        if (setNewSentance)
+                        {
+                            result = current;
+                        }
+
+                        current = "";
+                      
+                        setNewSentance = false;
+                    }
+                    else if (letter == '(')
+                    {
+                        count++;
+                        current += letter;
+                    }
+                    else if (letter == ')')
+                    {
+                        if (count > max)
+                        {
+                            setNewSentance = true;
+                            max = count;
+                        }
+
+                        current += letter;
+                        count--;
+                    }
+                    else
+                    {
+                        current += letter;
+                    }
+                }
+
+                sentence = sr.ReadLine();
+            }
+
+
+            sr.Close();
+            return result;
+        }
+
+        public static List<string> SortString(string filePath)
+        {
+            StreamReader streamReader =new StreamReader(filePath);
+            List<string> listStrings = new List<string>();
+            string current = "";
+            string line = streamReader.ReadLine();
+            while (line != null)
+            {
+                foreach (char letter in line)
+                {
+                    current += letter;
+
+                    if (letter == '.')
+                    {
+                        listStrings.Add(current);
+                        current = "";
+                    }
+                }
+                line = streamReader.ReadLine();
+            }
+            streamReader.Close();
+            listStrings.Sort((s1, s2) => s1.Length.CompareTo(s2.Length));
+            return listStrings;
+        }
+    }
+}
